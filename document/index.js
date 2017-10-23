955b0f64a31e18a0a5081d2e1533831b7a4f0721
Dibran Isufi <d.isufi@gmail.com>
2017-10-24 01:41:23 +0200

    feat: resolve includes
    note that you need the relevant branch of the framework linked to get this currently working

--- document/index.js
+++ document/index.js
@@ -2,7 +2,7 @@ require('coffee-script/register')
 const utils = require('./utils')
 
 const document = {
-  create ({design, content, referenceResolver, includeResolver}) {
+  create ({design, content}) {
     const framework = require('@livingdocs/framework')
     framework.design.load(design)
 
@@ -10,13 +10,28 @@ const document = {
     return doc
   },
 
-  render (doc) {
-    return doc.toHtml()
-  },
-
   visit (doc, filter, visitor) {
     utils.visit(doc.componentTree, filter, visitor)
     return doc
+  },
+
+  async resolveIncludes (doc, includeResolver) {
+    const accumulator = []
+    doc.componentTree.each((component) => {
+      component.directives.eachInclude((includeDirective) => {
+        accumulator.push(includeDirective)
+      })
+    })
+    for (const include of accumulator) {
+      const includeContent = include.getContent()
+      const includeHtml = await includeResolver(includeContent)
+      include.resolve(includeHtml)
+    }
+    return doc
+  },
+
+  render (doc) {
+    return doc.render()
   }
 }
 
