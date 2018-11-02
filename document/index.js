61528602bcfe1121dd2202359a4acb8fd7912b7d
Lukas <lpeyer@gmail.com>
2018-11-03 00:22:13 +0100

    chore(document-api): add some preliminary non-functional ideas

--- document/index.js
+++ document/index.js
@@ -1,10 +1,20 @@
 const utils = require('./utils')
 
 const document = {
+
+  // @param design {Object}
+  //   Serialized Livingdocs Design.
+  // @param content {Object}
+  //   Serialized Livingdocs Document.
+  // @param config {Object}
   create ({design, content, config}) {
     const framework = require('../framework/livingdocs-framework')
+
+    // todo: prevent global cache reset (LP)
     framework.design.resetCache()
     framework.design.load(design)
+
+    // todo: whitelist config properties that can be used here (LP)
     framework.config(config)
 
     const doc = framework.create({content, design})
@@ -34,8 +44,42 @@ const document = {
     return componentRenderer.renderComponent(component)
   },
 
+  // Render a Livingdoc Document.
+  // @param {Object}
+  //   Livingdocs Document. E.g. built with the `create()` method.
+  //
+  // @returns {String}
   render (doc) {
     return doc.render()
+  },
+
+  // @return {Array of String}
+  //   Returns the urls of all stylesheets that should be loaded when
+  //   `doc` is displayed in a browser.
+  getStylesheets ({doc}) {
+    // -> merge with dependencies of `doc`?
+    return doc.desgin.dependencies.css
+  },
+
+  getScripts ({doc}) {
+    // -> merge with dependencies of `doc`?
+    return doc.desgin.dependencies.js
+  },
+
+  // todo: Add a method to render a complete HTML document
+  // (with <head> and <body>) (LP)
+  // This document should include the required stylesheets and scripts.
+  renderHtmlDocument ({doc}) {
+    const cssStr = doc.design.dependencies.printCss()
+    const jsStr = doc.design.dependencies.printJs()
+
+    doc.render({
+      head: {
+        'og:image': '/foo.jpg'
+      },
+      appendToHead: [cssStr, jsStr],
+      includeDesignAssets: true
+    })
   }
 }
 
