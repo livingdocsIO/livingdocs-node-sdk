db7f862718a19c39a1d45237a7c956c7e9063b64
mokimo <bannedjobs@gmail.com>
2018-12-07 17:11:22 +0100

    fix(refactor): use function() instead of () =>

--- test/client/client-test.js
+++ test/client/client-test.js
@@ -10,61 +10,61 @@ const defaultConfig = {
 
 const createClient = config => new Client(config)
 
-describe('client', () => {
+describe('client', function () {
   const client = createClient(defaultConfig)
-  it('should be an object', () => {
+  it('should be an object', function () {
     expect(client).to.be.an('object')
   })
 
-  it('should always create a new instance', () => {
+  it('should always create a new instance', function () {
     const clientClone = createClient(defaultConfig)
     expect(clientClone).to.not.equal(client)
   })
 
-  it('should not accept an undefined or null config', () => {
-    expect(() => {
+  it('should not accept an undefined or null config', function () {
+    expect(function () {
       createClient()
     }).to.throw(Error, 'Config')
   })
 
-  it('should not accept an undefined or null API token', () => {
-    expect(() => {
+  it('should not accept an undefined or null API token', function () {
+    expect(function () {
       createClient({})
     }).to.throw(Error, 'API accessToken')
   })
 
-  it('should not accept an undefined or null url', () => {
-    expect(() => {
+  it('should not accept an undefined or null url', function () {
+    expect(function () {
       createClient({accessToken: API_TOKEN})
     }).to.throw(Error, 'Url')
   })
 
-  it('should have a service-property', () => {
+  it('should have a service-property', function () {
     expect(client).to.have.property('service')
   })
 
-  describe('publication', () => {
-    it('should exist', () => {
+  describe('publication', function () {
+    it('should exist', function () {
       expect(client.service).to.have.property('latestPublication')
     })
-    it('should not find an undefined publication', async () => {
+    it('should not find an undefined publication', async function () {
       const publication = await client.service.latestPublication(4963)
       expect(publication.error).to.equal('Not Found')
       expect(publication.status).to.equal(404)
     })
   })
-  describe('publications', () => {
-    it('should exist', () => {
+  describe('publications', function () {
+    it('should exist', function () {
       expect(client.service).to.have.property('latestPublications')
     })
-    it('Should have more than 2 entries', async () => {
+    it('Should have more than 2 entries', async function () {
       // The account(accessToken) you use, needs to have atleast 3 entries
       const publications = await client.service.latestPublications({
         limit: 3
       })
       expect(publications).to.have.length.above(2)
     })
-    it('should contain systemdata', async () => {
+    it('should contain systemdata', async function () {
       const publications = await client.service.latestPublications({
         limit: 2
       })
@@ -72,7 +72,7 @@ describe('client', () => {
         expect(publication).to.have.property('systemdata')
       })
     })
-    it('should contain metadata', async () => {
+    it('should contain metadata', async function () {
       const publications = await client.service.latestPublications({
         limit: 2
       })
@@ -80,7 +80,7 @@ describe('client', () => {
         expect(publication).to.have.property('metadata')
       })
     })
-    it('should contain content', async () => {
+    it('should contain content', async function () {
       const publications = await client.service.latestPublications({
         limit: 2
       })
